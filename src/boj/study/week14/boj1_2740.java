e2bef382b166ee0b717cbd471c059b1f281eb572
osjkate <osjkate@sookmyung.ac.kr>
2024-06-20 11:26:09 +0900

    [boj]

---
+++ src/boj/study/week14/boj1_2740.java
@@ -0,0 +1,50 @@
+package boj.study.week14;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj1_2740 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        br.readLine();
+
+        int[][] B = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] C = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                bw.write(C[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.close();
+    }
+}
