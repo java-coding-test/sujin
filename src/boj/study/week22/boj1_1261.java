7e7ebd47f2d26d8c8f35cd04ef4905f1f6cf444a
osjkate <osjkate@sookmyung.ac.kr>
2024-09-09 11:25:07 +0900

    [boj] 다익스트라 (1261)

---
+++ src/boj/study/week22/boj1_1261.java
@@ -0,0 +1,79 @@
+package boj.study.week22;
+
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class boj1_1261 {
+    static int N, M;
+    static int[][] graph, dist;
+    static int INF = Integer.MAX_VALUE;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new int[M + 1][N + 1];
+        dist = new int[M + 1][N + 1];
+        String str;
+        for (int i = 1; i < M + 1; i++) {
+            str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                graph[i][j + 1] = str.charAt(j) - '0';
+                dist[i][j + 1] = INF;
+            }
+
+        }
+        dijkstra();
+        bw.write(dist[M][N] + "");
+        bw.close();
+    }
+
+    public static void dijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(1, 1, 0));
+        dist[1][1] = 0;
+
+        int x, y;
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            x = current.x;
+            y = current.y;
+            if (x == M && y == N) break;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || nx > M || ny < 0 || ny > N) continue;
+
+                int newDist = dist[x][y] + graph[nx][ny];
+                if (newDist < dist[nx][ny]) {
+                    dist[nx][ny] = newDist;
+                    pq.offer(new Node(nx, ny, newDist));
+                }
+            }
+        }
+    }
+}
+
+class Node implements Comparable<Node> {
+    int x, y, cost;
+
+    public Node(int x, int y, int cost) {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+    }
+
+
+    @Override
+    public int compareTo(Node o) {
+        return Integer.compare(this.cost, o.cost);
+    }
+}
